21e0469aafb801cc1e0fc55addb7b759add4d367
Soumya Deb <debloper@gmail.com>
2014-06-19 23:40:16 +0530

    Working prototype of /apps/create API - Expects formatted JSON request - Returns WebApp ID as response
    Signed-off-by: Soumya Deb <debloper@gmail.com>

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,45 @@
 var express = require("express"),
+    uuid = require("shortid"),
     config = require("../config"),
     db = require("../db");
 
-var router = express.Router();
-
+var router = express.Router(),
+    apps = db.get("apps");
 
 router.post("/apps/create", function (req, res) {
-    var appname = req.body && req.body.name,
-        appdescription = req.body && req.body.description,
-        appurl = req.body && req.body.url.replace(/\/$/, "");
+    var appName = req.body && req.body.name,
+        appDesc = req.body && req.body.description,
+        appUrl  = req.body && req.body.url.replace(/\/$/, ""),
+        appId   = uuid.generate();
 
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-    if (appname && appurl && appdescription) {
-        res.json(200, {message: "Ok"});
+    if (appName && appUrl && appDesc) {
+        apps.insert({
+            manifest: {
+                name: appName,
+                description: appDesc,
+                launch_path: appUrl,
+                icons: {
+                   "60": "/assets/img/icon_60.png",
+                   "128": "/assets/img/icon_128.png"
+                },
+                developer: {
+                name: "Applait",
+                    url: "http://applait.io/"
+                },
+                default_locale: "en"
+            },
+            id: appId
+        }, function (error) {
+            if (error) {
+                res.json(500, "Oh, noes... couldn't _insert_ it!");
+            } else {
+                res.json(200, {message: appId});
+            }
+        });
     } else {
         res.json(500, {message: "Invalid data provided."});
     }
