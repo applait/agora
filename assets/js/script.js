702d599ac84bf3234c9ed6534ef508b1396b1ec1
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-06-30 14:25:27 +0530

    Wrap scripts in document.ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,88 @@
-// Configuration parapmeters
-var endPoint = "/api/apps/create",
-    requestbase = "http://request-applait.rhcloud.com",
-    appId,
-    appurl,
-    manifesturl;
+$(document).ready( function ($) {
+    // Configuration parapmeters
+    var endPoint = "/api/apps/create",
+        requestbase = "http://request-applait.rhcloud.com",
+        appId,
+        appurl,
+        manifesturl;
 
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('.page-scroll a').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-// Handle app-request with AJAX
-var youGoBoy = function (appname, appdescription, url) {
-    $.ajax({
-        type: "POST",
-        url: endPoint,
-        data: {
-            name: appname,
-            description: appdescription,
-            url: url
-        }
-    }).done(function (data) {
-        appId = data.appId;
-        appurl = "/apps/" + appId;
-        manifesturl = "/api/apps/" + appId + "?prettyprint=true";
-        $("#submission-toast").html($("#submission-toast").html()
-                .replace(/\{appurl\}/, appurl)
-                .replace(/\{manifesturl\}/, manifesturl))
-            .fadeIn();
-        $("#generator-form").remove();
-
-        $("#request-submit").click( function (event) {
+    //jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(function() {
+        $('.page-scroll a').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
             event.preventDefault();
-            var email = $("#item-email").val().trim();
-            if (email && /\w+\@\w+\.\w+/.test(email)) {
-                $.ajax({
-                    type: "POST",
-                    url: requestbase,
-                    data: {
-                        email: email,
-                        appId: appId
-                    }
-                }).done(function (data) {
-                    $("#request-form").remove();
-                    $("#request-success").removeClass("hidden");
-                });
-            } else {
-                $("#item-email").select();
-            }
-            return false;
         });
-
-    }).fail(function (error) {
-        console.log(JSON.stringify(error, null, "  "));
     });
-};
 
-var ohNoes = function () {
-    $("#failure-toast").fadeIn();
-    $("#generator-form").hide();
-}
+    // Handle app-request with AJAX
+    var youGoBoy = function (appname, appdescription, url) {
+        $.ajax({
+            type: "POST",
+            url: endPoint,
+            data: {
+                name: appname,
+                description: appdescription,
+                url: url
+            }
+        }).done(function (data) {
+            appId = data.appId;
+            appurl = "/apps/" + appId;
+            manifesturl = "/api/apps/" + appId + "?prettyprint=true";
+            $("#submission-toast").html($("#submission-toast").html()
+                    .replace(/\{appurl\}/, appurl)
+                    .replace(/\{manifesturl\}/, manifesturl))
+                .fadeIn();
+            $("#generator-form").remove();
 
-$("#generator-submit").click( function (event) {
-    event.preventDefault();
-    var appname = $("#item-app-name").val();
-    var appdescription = $("#item-app-description").val();
-    var url = $("#item-url").val();
-    (appname && appdescription && url) ? youGoBoy(appname, appdescription, url) : ohNoes();
-    return false;
-});
+            $("#request-submit").click( function (event) {
+                event.preventDefault();
+                var email = $("#item-email").val().trim();
+                if (email && /\w+\@\w+\.\w+/.test(email)) {
+                    $.ajax({
+                        type: "POST",
+                        url: requestbase,
+                        data: {
+                            email: email,
+                            appId: appId
+                        }
+                    }).done(function (data) {
+                        $("#request-form").remove();
+                        $("#request-success").removeClass("hidden");
+                    });
+                } else {
+                    $("#item-email").select();
+                }
+                return false;
+            });
+
+        }).fail(function (error) {
+            console.log(JSON.stringify(error, null, "  "));
+        });
+    };
+
+    var ohNoes = function () {
+        $("#failure-toast").fadeIn();
+        $("#generator-form").hide();
+    }
+
+    $("#generator-submit").click( function (e) {
+        e.preventDefault();
+        var appname = $("#item-app-name").val();
+        var appdescription = $("#item-app-description").val();
+        var url = $("#item-url").val();
+        (appname && appdescription && url) ? youGoBoy(appname, appdescription, url) : ohNoes();
+    });
 
 
 
-// Reenable form, for resubmission
-$(".request-again").click( function (event) {
-    $(this).parent().hide();
-    $("#generator-form").fadeIn();
-    $("#item-app-name").select();
+    // Reenable form, for resubmission
+    $(".request-again").click( function (event) {
+        $(this).parent().hide();
+        $("#generator-form").fadeIn();
+        $("#item-app-name").select();
+    });
+
 });
