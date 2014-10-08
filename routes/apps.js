9eecedea6e763f76b817b7f7ad08d500bfc72a36
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2014-10-09 02:54:38 +0530

    Customize title for app detail page and fix deprecated api cal
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- routes/apps.js
+++ routes/apps.js
@@ -6,12 +6,13 @@ var router = express.Router(),
     apps = db.get("apps");
 
 router.get("/", function (req, res) {
-    res.json(200, {"message": "Here be apps"});
+    res.status(200).json({"message": "Here be apps"});
 });
 
 router.get("/:id", function (req, res) {
     apps.findOne({ appId: req.params.id}).on("success", function (doc) {
         doc.config = agora.config;
+        doc.title = doc.manifest.name + " - " + doc.appId + " | Applait Explore";
         res.render("apps/detail.html", doc);
     });
 });
